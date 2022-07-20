07688e70cab2366dd55442acb74b7b127ab54201
David Nyan <DavidNyan211106@gmail.com>
2022-07-20 21:11:08 +0630

    fix

--- src/data.js
+++ src/data.js
@@ -32,3 +32,70 @@ export const projects = [
 		link: "https://stackoverflow.com"
 	}
 ];
+
+export const skills = [
+	{
+		title: "HTML",
+		subtitle: "h",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./skill-1.gif",
+		link: "https://google.com"
+	},
+	{
+		title: "CSS",
+		subtitle: "c",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./skill-2.gif",
+		link: "https://nhentai.net"
+	},
+	{
+		title: "JavaScript",
+		subtitle: "j",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./skill-3.gif",
+		link: "https://hanime.tv"
+	},
+	{
+		title: "React",
+		subtitle: "r",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./skill-4.gif",
+		link: "https://pornhub.com"
+	},
+	{
+		title: "Python",
+		subtitle: "p",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./skill-5.gif",
+		link: "https://xnxx.com"
+	}
+];
+
+export const testimonials = [
+	{
+		name: "John Doe",
+		title: "CEO",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./testimonial-1.gif"
+	},
+	{
+		name: "Jane Doe",
+		title: "CTO",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./testimonial-2.gif"
+	},
+	{
+		name: "Joe Doe",
+		title: "COO",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+		image: "./testimonial-3.gif"
+	}
+];
